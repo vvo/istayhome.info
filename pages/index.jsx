6345a850b83eea6f36edcaee71d7a47cf54074d9
Vincent Voyer <vincent@codeagain.com>
2020-03-16 01:38:42 +0100

    fix lang detection

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,7 +6,7 @@ export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
-    router.push(`/${navigator.language}`);
+    router.push(`/${navigator.language.slice(0, 2)}`);
   });
 
   return (
